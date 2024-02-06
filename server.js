a330d02402225097436d4232ef69db67cc67d71c
Jordan <jkimys2@gmail.com>
2024-02-06 10:56:22 -0500

    server file update

--- server.js
+++ server.js
@@ -11,3 +11,6 @@ PORT = process.env.PORT || 3001;
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// app listener to start server
+app.listen(PORT, () => console.log(`Note Taker app listening at http://localhost:${PORT}!`))
