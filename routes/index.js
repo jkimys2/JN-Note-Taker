bb1c06257b71740e53fbd407e0439309ec246878
Jordan <jkimys2@gmail.com>
2024-02-06 12:40:51 -0500

    index router update

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 // import dependencies
-const apiRouter = require('express').Router();
+const apiRouter = require("express").Router();
 
 // import files containing routes
 const notesRouter = require("./notes");
-const htmlRouter = require("./html");
 
 apiRouter.use("/notes", notesRouter);
+
+// export apiRouter
+module.exports = apiRouter;
