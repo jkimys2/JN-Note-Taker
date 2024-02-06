32f45f4f2a41ae43d19974c42292da1c5720faa4
Jordan <jkimys2@gmail.com>
2024-02-06 12:54:31 -0500

    server file update

--- server.js
+++ server.js
@@ -15,11 +15,14 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+// Send all requests that begin with /api to router file
+app.use("/api", apiRouter);
+
 // GET route to navigate to notes.html from the index.html
 app.get("/notes", (req, res) => {
-  console.info(`${req.method} request made`);
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });  
 
 // GET route to return to index.html when user attempts to visit routes that don't exist
 app.get("*", (req, res) => {
