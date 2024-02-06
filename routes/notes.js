7569ced089ab1a91336a57bf8c61b8122a6fadf9
Jordan <jkimys2@gmail.com>
2024-02-06 12:12:18 -0500

    note router update

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,47 @@
 // import dependencies
-const notesRouter = require('express').Router();
-const path = require('path');
-const uuid = require('uuid');
+const notesRouter = require("express").Router();
+const fs = require("fs");
+const path = require("path");
+const uuid = require("uuid");
 
 // GET route for retrieving data
-notesRouter.get('/', (req, res => {
+notesRouter.get(
+  "/",
+  (req,
+  (res) => {
     console.info(`${req.method} request received for notes`);
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-}));
+    res.sendFile(path.join(__dirname, "../db/db.json"));
+  })
+);
 
 // POST route for submitting note
-notesRouter.post('/', (req, res))
+notesRouter.post("/", (req, res) => {
+  console.info(`${req.method} request received to submit note`);
+
+  // destructuring assignment for items in req.body
+  const { title, text } = req.body;
+
+  // Save if all the required properties are present
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    fs.readFile("../db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(newNote);
+        const stringedNote = JSON.stringify(newNote);
+        fs.writeFile("../db/db.json", stringedNote, (error) =>
+          err
+            ? console.log(error)
+            : console.log(`New note has been written to JSON file!`)
+        );
+      }
+    });
+  }
+});
